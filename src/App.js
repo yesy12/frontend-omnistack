85be7b10fcb91bf7aa21b364d2ebc392b965245a
Alison <alisonvieira.av29@gmail.com>
2019-10-03 20:25:08 -0300

    css e logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import "./App.css";
+import logo from "./assets/logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <h1> Hello World </h1>
+    <div className="containter">
+      <img src={logo} alt=""/>
     </div>
   );
 }
