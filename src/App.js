4b75d4c1c97371615a009f32f63312b85ced6f48
Alison <alisonvieira.av29@gmail.com>
2019-10-03 20:32:27 -0300

    label, input, button

--- src/App.js
+++ src/App.js
@@ -11,7 +11,20 @@ function App() {
         <p> 
           Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
         </p>
+        <form>
+          <label htmlFor="email"> 
+            E-Mail *
+          </label>
 
+          <input 
+            type="email" 
+            id="email" 
+            name="email" 
+            placeholder="Seu melhor e-mail" />
+
+          <button type="submit">Entrar</button>
+
+        </form>
       </div>
     </div>
   );
