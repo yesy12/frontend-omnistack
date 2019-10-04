19b0bda3508f9d4e7694bb8633c445d08bc0a552
Alison <alisonvieira.av29@gmail.com>
2019-10-04 18:54:35 -0300

    Movido de App.js para Login/index.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import "./App.css";
 import logo from "./assets/logo.svg";
 
 import api from "./services/api";
 
+import Routes from "./Routes";
+
 function App() {
-  const [email,setEmail] = useState("")
-
-   const handleSubmit = async (event) =>{
-    event.preventDefault();
-    
-    const response = await api.post("/users",{
-      email
-    })
-
-    const { _id } = response.data;
-    
-    localStorage.setItem("user",_id);
-
-    
-  }
   
-  const switchEmail = (event) =>{
-    setEmail(event.target.value);
-  } 
 
 
   return (
@@ -31,25 +15,7 @@ function App() {
       <img src={logo} alt="AirCnc"/>
 
       <div className="content">
-        <p> 
-          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
-        </p>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email"> 
-            E-MAIL *
-          </label>
-
-          <input 
-            type="email" 
-            id="email" 
-            name="email" 
-            value={email}
-            onChange={switchEmail}
-            placeholder="Seu melhor e-mail" />
-
-          <button className="btn" type="submit">Entrar</button>
-
-        </form>
+        <Routes />
       </div>
     </div>
   );
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+    const [email,setEmail] = useState("")
+
+    const handleSubmit = async (event) =>{
+        event.preventDefault();
+        
+        const response = await api.post("/users",{
+          email
+        })
     
+        const { _id } = response.data;
+        
+        localStorage.setItem("user",_id);
+    
+        
+      }
+      
+      const switchEmail = (event) =>{
+        setEmail(event.target.value);
+      } 
+
+    return (
+        <>
+            <p> 
+                Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
+            </p>
+
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="email"> 
+                E-MAIL *
+                </label>
+
+                <input 
+                type="email" 
+                id="email" 
+                name="email" 
+                value={email}
+                onChange={switchEmail}
+                placeholder="Seu melhor e-mail" />
+
+                <button className="btn" type="submit">Entrar</button>
+
+            </form>
+      </>
+    )
 }
 
 export default Login;
