2eccf404eaf46e7fee3c44ffd8e41dba778baec7
Alison <alisonvieira.av29@gmail.com>
2019-10-04 20:38:00 -0300

    form de new

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,9 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 
 const New = () => {
+    const [company,setCompany ] = useState("");
+    const [techs,setTechs] = useState("");
+    const [price,setPrice] = useState("");
+
+    const handleSubmit = () => {
+
+    }
+
+
+    const setCompanyInfo = ( event ) => {
+        setCompany(event.targer.value);
+    }
+
+    const setTechsInfo = ( event ) => {
+        setTechs(event.targer.value);
+    }
+
+    const setPriceInfo = ( event ) => {
+        setPrice(event.targer.value);
+    }
+
+
     return (
         <>
-            <h1>New</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="company"> EMPRESA *</label>
+                <input 
+                    type="text" 
+                    id="company" 
+                 placeholder="Sua empresa incrível"
+                   value={company}
+                   onChange={setCompanyInfo}
+                />
+
+                <label htmlFor="techs"> 
+                    Tecnologias *  
+                    <span>
+                        (separadas por vírgula) 
+                    </span>
+                </label>
+                <input 
+                    type="text" 
+                    id="techs" 
+                    placeholder="Quais tecnlogias usam?"
+                    value={techs}
+                    onChange={setTechsInfo}
+                />
+
+                <label htmlFor="price">
+                    VALOR DA DIÁRIA 
+                    <span>
+                        (em branco para GRATUITO) 
+                    </span>
+                </label>
+                <input 
+                  type="text" 
+                    id="company" 
+                    placeholder="Valor cobrado por dia"
+                    value={price}
+                    onChange={setPriceInfo}
+                />
+
+
+            </form>
         </>
     )
 }
