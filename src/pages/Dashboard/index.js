e5e3656442baf4c9d34f9c6dc025cbd5aeb669cb
Alison <alisonvieira.av29@gmail.com>
2019-10-04 19:07:08 -0300

    useEffect

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Dashboard = () => {
+    useEffect(()=>{
+
+    },[])
+    
     return (
         <>
             <h1> Dashboard </h1>
+
         </>
     )
 }
