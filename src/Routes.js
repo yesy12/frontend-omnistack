935c54348f9c2deabff1024711e903061ac8395a
Alison <alisonvieira.av29@gmail.com>
2019-10-04 18:43:27 -0300

    Sistema de rota

--- src/Routes.js
+++ src/Routes.js
@@ -9,7 +9,15 @@ import Dashboard from "./pages/Dashboard/index";
 import New from "./pages/New/index";
 
 const Routes = () =>{
-
+    return (
+        <Router>
+            <Switch>
+                <Route path="/" exact component={Login}/>
+                <Route path="/dashboard" exact component={Dashboard}/>
+                <Route path="/new" exact component={New}/>
+            </Switch>
+        </Router>
+    );
 }
 
 export default Routes;
