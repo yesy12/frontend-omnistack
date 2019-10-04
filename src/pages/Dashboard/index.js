e1f99bff22489f5f2ec35485f4f3b069fb8474d5
Alison <alisonvieira.av29@gmail.com>
2019-10-04 19:15:22 -0300

    funcao async/await

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,10 +1,24 @@
 import React, { useEffect } from "react";
+import api from "../../services/api";
+import { loadOptions } from "@babel/core";
 
 const Dashboard = () => {
+        
     useEffect(()=>{
+        const loadSpots = async () =>{
+            const user_id = localStorage.getItem("user");
+            const response = await api.get("/dashboard",{
+                headers : { user_id }
+            })
 
+
+            console.log(response.data)
+        }
+        
+
+        loadSpots();
     },[])
-    
+
     return (
         <>
             <h1> Dashboard </h1>
