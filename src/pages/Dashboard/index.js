e33d348fbe06ec54f1fd0ce66d1d89e3283276ee
Alison <alisonvieira.av29@gmail.com>
2019-10-04 20:11:57 -0300

    importado e criado style.css

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,6 +1,8 @@
 import React, { useEffect,useState } from "react";
 import api from "../../services/api";
 
+import "./style.css";
+
 const Dashboard = () => {
     const [spots,setSpots] = useState([])
 
